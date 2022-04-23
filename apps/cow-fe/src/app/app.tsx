af9e89e35865d3be49a20789d6d80fcdf7710236
fatih.dogmus <fdogmus06@gmail.com>
2022-04-23 14:44:37 +0300

    initial commit

---
+++ apps/cow-fe/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="cow-fe" />
+      <div />
+    </>
+  );
+}
+
+export default App;
