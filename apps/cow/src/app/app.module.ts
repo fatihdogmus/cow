c869e694c56b04b2559a815bb5d6de73e138444f
fatihdogmus <fdogmus06@gmail.com>
2022-05-15 15:16:55 +0300

    #1 Connected express session to redis

--- apps/cow/src/app/app.module.ts
+++ apps/cow/src/app/app.module.ts
@@ -1,21 +1,24 @@
-import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { Inject, MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { DatasourceModule } from "./configuration/datasource/datasource.module";
 import { AuthenticationModule } from "./authentication/authentication.module";
 import * as session from "express-session";
 import * as passport from "passport";
+import { REDIS, RedisModule } from "./configuration/redis/redis.module";
+import * as createRedisStore from "connect-redis";
+import { Client } from "connect-redis";
 
 @Module({
-  imports: [DatasourceModule, AuthenticationModule]
+  imports: [DatasourceModule, AuthenticationModule, RedisModule]
 })
 export class AppModule implements NestModule {
-  // constructor(@Inject(REDIS) private readonly redis: Client) {}
+  constructor(@Inject(REDIS) private readonly redis: Client) {}
 
   configure(consumer: MiddlewareConsumer) {
-    // const RedisStore = createRedisStore(session);
+    const RedisStore = createRedisStore(session);
     consumer
       .apply(
         session({
-          // store: new RedisStore({ client: this.redis, logErrors: true }),
+          store: new RedisStore({ client: this.redis, logErrors: true }),
           saveUninitialized: false,
           secret: "sup3rs3cr3t",
           resave: false,
