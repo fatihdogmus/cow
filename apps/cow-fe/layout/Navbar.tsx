eca0b8db8834eef561f3e741ac94a518e21dccf5
fatihdogmus <fdogmus06@gmail.com>
2022-05-16 22:14:47 +0300

    #6 Started creating log-in form

--- apps/cow-fe/layout/Navbar.tsx
+++ apps/cow-fe/layout/Navbar.tsx
@@ -1,10 +1,27 @@
-import { AppBar, Avatar, Badge, Box, IconButton, Toolbar, Tooltip, useTheme } from "@mui/material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  Button,
+  Container,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  IconButton,
+  TextField,
+  Toolbar,
+  Tooltip,
+  useTheme
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PeopleIcon from "@mui/icons-material/People";
 import styled from "@emotion/styled";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface Props {
   onSidebarOpen: () => void;
@@ -19,6 +36,22 @@ const DashboardNavbarRoot = styled(AppBar)(() => {
 });
 export const Navbar = (props: Props) => {
   const { onSidebarOpen, ...other } = props;
+
+  const [isSignInModalOpen, setSignInModalOpen] = useState(false);
+
+  function openSignInModal() {
+    setSignInModalOpen(true);
+  }
+
+  function closeSignInModal() {
+    return setSignInModalOpen(false);
+  }
+
+  const { register, handleSubmit, formState, control } = useForm({});
+
+  function submit(values) {
+    console.log(values);
+  }
 
   return (
     <>
@@ -67,6 +100,11 @@ export const Navbar = (props: Props) => {
               </Badge>
             </IconButton>
           </Tooltip>
+          <Tooltip title="Sign-in">
+            <Button onClick={openSignInModal} variant="contained">
+              Sign-in
+            </Button>
+          </Tooltip>
           <Avatar
             sx={{
               height: 40,
@@ -78,6 +116,45 @@ export const Navbar = (props: Props) => {
           </Avatar>
         </Toolbar>
       </DashboardNavbarRoot>
+      <Dialog open={isSignInModalOpen} onClose={closeSignInModal}>
+        <DialogTitle>Sign-in</DialogTitle>
+        <DialogContent>
+          <Box
+            component="main"
+            sx={{
+              alignItems: "center",
+              display: "flex",
+              flexGrow: 1,
+              minHeight: "100%"
+            }}>
+            <Container maxWidth="sm">
+              <form onSubmit={handleSubmit(submit)}>
+                <TextField
+                  fullWidth
+                  label="Email Address"
+                  margin="normal"
+                  name="email"
+                  type="email"
+                  variant="outlined"
+                />
+                <TextField
+                  fullWidth
+                  label="Password"
+                  margin="normal"
+                  name="password"
+                  type="password"
+                  variant="outlined"
+                />
+                <Box sx={{ py: 2 }}>
+                  <Button color="primary" fullWidth size="large" type="submit" variant="contained">
+                    Sign In Now
+                  </Button>
+                </Box>
+              </form>
+            </Container>
+          </Box>
+        </DialogContent>
+      </Dialog>
     </>
   );
 };
--- apps/cow-fe/pages/_document.tsx
+++ apps/cow-fe/pages/_document.tsx
@@ -1,36 +1,7 @@
-import React, { Children } from "react";
+import React from "react";
 import Document, { Head, Html, Main, NextScript } from "next/document";
-import { createEmotionCache } from "../utils/create-emotion-cache";
-import createEmotionServer from "@emotion/server/create-instance";
 
 class _document extends Document {
-  getInitialProps = async ctx => {
-    const originalRenderPage = ctx.renderPage;
-    const cache = createEmotionCache();
-    const { extractCriticalToChunks } = createEmotionServer(cache);
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props => <App emotionCache={cache} {...props} />
-      });
-
-    const initialProps = await Document.getInitialProps(ctx);
-    const emotionStyles = extractCriticalToChunks(initialProps.html);
-    const emotionStyleTags = emotionStyles.styles.map(style => (
-      <style
-        data-emotion={`${style.key} ${style.ids.join(" ")}`}
-        key={style.key}
-        // eslint-disable-next-line react/no-danger
-        dangerouslySetInnerHTML={{ __html: style.css }}
-      />
-    ));
-
-    return {
-      ...initialProps,
-      styles: [...Children.toArray(initialProps.styles), ...emotionStyleTags]
-    };
-  };
-
   render(): JSX.Element {
     return (
       <Html>
