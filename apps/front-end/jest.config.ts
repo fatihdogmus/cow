de77f4266713637f82b7f1d678bc57936970617c
fatihdogmus <fdogmus06@gmail.com>
2022-05-16 22:18:41 +0300

    #6 Renamed cow-fe to front-end

--- apps/front-end/jest.config.ts
+++ apps/front-end/jest.config.ts
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: "cow-fe",
+  displayName: "front-end",
   preset: "../../jest.preset.ts",
   transform: {
     "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
     "^.+\\.[tj]sx?$": "babel-jest"
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-  coverageDirectory: "../../coverage/apps/cow-fe"
+  coverageDirectory: "../../coverage/apps/front-end"
 };
